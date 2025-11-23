007f196bec39d063fa8267b37070b97a3932d5fe
Nikolaenko <aleshka.dev@gmail.com>
2025-11-23 13:32:18 +0200

    add validate user method

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { User } from 'src/user/entity/user.entity';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [AuthService, UserService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { verify } from 'argon2';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async validate(email: string, password: string) {
+    const user = await this.userService.getByEmail(email);
+    const isValid = await verify(password, user.password);
+
+    if (!isValid) {
+      throw new UnauthorizedException('Невірний пароль.');
+    }
+
+    return user;
+  }
+}
